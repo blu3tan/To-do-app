21633a8fc9c53ade6469d3610f189644acda1e28
blu3tan <142602465+blu3tan@users.noreply.github.com>
2024-01-17 21:06:56 +0100

    Add button style and animation

--- src/todo-form-creation.js
+++ src/todo-form-creation.js
@@ -3,11 +3,12 @@ let formStatus = 'closed';
 
 const formMarkup =  `<form action="#" method="dialog" class="form-fields">
 <div class="form-row"><label for="title">What?</label><input id="title" name="title" type="text" maxlength="20" placeholder="Title" required></div>
-<div class="form-row"><label for="due-date">When?</label><input type="date" id="date" name="due-date"/></div>
+<div class="form-row"><label for="due-date">When?</label><input type="date" id="date" name="due-date" required/></div>
 <div class="form-row"><label for="priority">Priority</label><select id="priority" name="priority"><option value="low">Low</option><option value="med">Med</option><option value="high">High</option></select></div>
 <div class="form-row area"><label for="description">How?</label>
 <textarea id="description" name="description" maxlength="200"></textarea></div>
-<div class="form-row"></div></form>`
+<div class="form-row"><label for="tags">Tag</label><select id="tags" name="tags"><option value="all">All</option><option value="work">Work</option><option value="sport">Sport</option></select></div><div class="form-row"><label for="todoBtn" class="todoLabel">_</label><button id="set-todo" type="button" class="set-btn" name="todoBtn">
+<span class="set-btn-txt">SET&nbsp;&nbsp;TODO</span></button></div></form>`
 
 export function todoFormCreation() {
     
