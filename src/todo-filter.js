eab313a6c91ad3a8ecdf50130eb33b7ede9f6a1a
blu3tan <142602465+blu3tan@users.noreply.github.com>
2024-01-24 21:48:28 +0100

    Remove commented code

--- src/todo-filter.js
+++ src/todo-filter.js
@@ -4,7 +4,9 @@ import { renderTodoList } from "./index";
 
 export function filterListByTag(tag) {
     const todoContainer = document.querySelector('.todos');
-    // let filteredList = todoList.filter((item) => item.tag == tag);
+    // array.filter() method was generating an array with the correct items
+    // but with total different indexes. Instead with this forEach() function
+    // i get an array with empty slots keeping the original indexes
     let filtered = [];
     todoList.forEach((item) => {
         if (item.tag == tag) filtered[todoList.indexOf(item)] = item;
